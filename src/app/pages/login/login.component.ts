0e15fa6af5c8b75c9012aa6551fcefe83ab9c234
Phan Thanh Vu <dieter.vu@vela.com.vn>
2024-08-08 10:47:51 +0700

    Reapply "test: cherry pick 1"
    This reverts commit cc945716aaaeb77e9dd2179bd97ed7b3ca8c9c22.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -86,4 +86,7 @@ export class LoginComponent implements OnInit {
   onLanguageChanged(language: string): void {
     this.currentLanguage = language;
   }
+  onC1(){
+    console.log("Cherry pick C1")
+  }
 }
